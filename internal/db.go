03797186836f33679f7c1ef1d6d8c43a77e3e4ec
Yugo Ibuki <y.ibuki91@gmail.com>
2023-09-02 18:59:34 +0900

    chore: fix db pkg (#8)

--- internal/db.go
+++ internal/db.go
@@ -1,14 +1,31 @@
 package db
 
 import (
+	"entgo.io/ent/dialect/sql"
+	"fmt"
+	"github.com/yugo-ibuki/musdok/ent"
 	"os"
 )
 
 const (
-	path = "./musdok.db"
+	db      = "sqlite3"
+	dbName  = "musdok.db"
+	timeout = 5000
+	source  = "file:" + dbName + "?_fk=1&cache=shared&_busy_timeout=" + string(timeout)
 )
 
+func Client() (*ent.Client, error) {
+	drv, err := sql.Open(db, source)
+	if err != nil {
+		fmt.Println("failed to open driver: %v", err)
+		return nil, err
+	}
+
+	return ent.NewClient(ent.Driver(drv)), nil
+}
+
 func DatabaseExists() bool {
+	path := "./" + dbName
 	_, err := os.Stat(path)
 	if os.IsNotExist(err) {
 		return false
