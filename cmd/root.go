3dbbd1fc1b3e9acd6368315d33b6d354c68b70cd
--global <y.ibuki91@gmail.com>
2023-09-02 14:39:10 +0900

    feat: add init cmd

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+func newRootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "musdok",
+		Short: "musdok is a CLI to add and search for the todo list",
+		Long:  "musdok is a CLI to add and search for the todo list",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("hello world!!")
+		},
+	}
+	cmd.AddCommand(newInitCmd())
+	return cmd
+}
+
+func Execute() int {
+	if err := newRootCmd().Execute(); err != nil {
+		fmt.Println(err)
+		return 1
+	}
+
+	return 0
+}
