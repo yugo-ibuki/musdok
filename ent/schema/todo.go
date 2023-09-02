89d7b25debfd4bcf674466cb3b8292ce59fe9775
--global <y.ibuki91@gmail.com>
2023-09-02 16:27:13 +0900

    feat: add Todo entity and generate file

---
+++ ent/schema/todo.go
@@ -0,0 +1,30 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"time"
+)
+
+// Todo holds the schema definition for the Todo entity.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields of the Todo.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("id").Unique(),
+		field.Int("order").NonNegative(),
+		field.String("title").NotEmpty(),
+		field.String("description").NotEmpty(),
+		field.Bool("done").Default(false),
+		field.Time("created_at").Default(time.Now),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
+}
+
+// Edges of the Todo.
+func (Todo) Edges() []ent.Edge {
+	return nil
+}
